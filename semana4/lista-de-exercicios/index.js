744aa0b58b699fc16a2d0fce1aa1c89dcd112df4
Josewd <63750044+Josewd@users.noreply.github.com>
2020-10-09 10:57:17 -0300

    exercicios de interpretacao respondidos

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,41 @@
+/*EXERCICIOS DE INTERPRETACAO
+1-  declara uma funcao conversorDeMoeda que recebe como 
+parametro o valor em dolar.
+a funcao converte o valor recebio como parametro na quantidade
+equivalente em Reais de acordo com o valor de cotacao
+informado pelo usuario atraves do prompt criado dentro da funcao.
+
+no console sera impresso o resultado da multiplicacao de 100 * o valor guardado
+na variavel 'cotacao' 
+
+2- declara uma funcao que calcula um valor apos um investimento,
+a funcao recebe dois parametros: tipo de investimento e Valor
+para cada tipo de investimento ha um valor diferente a ser multiplicado
+pelo valor informado como parametro.
+se o tipo de investimento for incorreto ou nao reconhecido dentro da funcao,
+ela retorna um alert para o usuario.
+
+no console sera impresso:
+165
+TIPO DE INVESTIMENTO INFORMADO INCORRETO!
+
+3- declara 3 arrays, sendo uma com numeros aleatorios e duas vazias
+a seguir eh criado um loop for of para o array de numeros
+com o objetivo de separar numeros pares de impares atraves de condicoes if e else
+e adicionando atraves do metodo push numeros pares ao array1
+e numeros impares ao array2.
+Infelizmente o codigo nao rodara depois da linha seguinte ao if, pois 
+o codigo tenta mudar uma variavel constante adicionando algo nela.
+
+no console sera impresso os erros cometidos.
+
+4- declara um array de numeros e duas variaveis, numero1 guardando o valor infinity 
+e outra numero2 0.
+e criado um loop for of do array de numeros, em cada loop verifica-se
+se o numero atual e menor que numero1 se sim numero1 recebe o valor do numero,
+e se numero e maior que numero2 se sim numero2 receber o valor de numero.
+
+no console sera impresso:
+-10
+1590
+*/
