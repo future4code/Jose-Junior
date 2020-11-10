6f6be3db5691670e7998b2c60afb5591ec3ba9f4
Josewd <63750044+Josewd@users.noreply.github.com>
2020-11-10 19:39:59 -0300

    changes

--- semana8/projeto-astromatch/astromatch/src/Components/Matches.js
+++ semana8/projeto-astromatch/astromatch/src/Components/Matches.js
@@ -1,8 +1,8 @@
 import React, { useState , useEffect} from 'react';
 import styled from 'styled-components'
 import axios from 'axios'
-import {MainDiv }from './FindMatch'
-import Button from '@material-ui/core/Button';
+import {MainDiv, SingleMatch, Img, Name, DivButton, CustomButton }from './StyleFindMatch'
+
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import {HeartDiv} from '../Screens/Home'
 
@@ -10,39 +10,6 @@ const MainDivMatches = styled(MainDiv)`
     flex-direction: column;
 `
 
-const SingleMatch = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    margin: 3px;
-    width: 100%;
-    height: 60px;
-    z-index: 15;
-    transition: 1s ease;
-    &:hover{
-        background-color: rgba(0,0,0,0.2);
-    }
-`
-const Img = styled.img`
-    height: 60px;
-    width: 60px;
-    border-radius: 3px;
-`
-const Name = styled.h2`
-    margin-left: 10px;
-    font-family: 'Kaushan Script', cursive;
-`
-const DivButton = styled.div`
-    position: absolute;
-    bottom: 0px;
-    width: 100%;
-    align-self: flex-end;
-`
-const CustomButton = styled(Button)`
-    width: 100%;
-    font-family: 'Kaushan Script', cursive;
-
-`
 const clearMatches = ()=>{
     axios.put('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/junior/clear').then(response=>{
 
