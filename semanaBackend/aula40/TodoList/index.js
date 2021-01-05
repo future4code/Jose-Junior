e191b2f90f41c3c1b0eced33f621f544513e6e02
josewd <exageradonome@gmail.com>
2021-01-05 09:40:47 -0300

    changes

--- semanaBackend/aula40/TodoList/index.js
+++ semanaBackend/aula40/TodoList/index.js
@@ -7,5 +7,6 @@ fs.appendFile("./TodoList/todo.txt", `${process.argv[2]},\n`, (err)=> err&&conso
 fs.readFile("./TodoList/todo.txt", function(err, buf) {
     console.log('Tasks to do:\n' + buf.toString());
   });}else{
-      console.log("\x1b[31m",'Usage: npm run start "your-task-here"')
-  }
+         console.log("\x1b[31m",'Usage: npm run start "your-task-here"')
+  } 
+  
